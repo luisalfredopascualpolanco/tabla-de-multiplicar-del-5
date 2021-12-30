abb8f00aac34f05cad4a3475fb145cb4b689fd44
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-30 11:18:42 -0800

    version1.0

---
+++ proyecto10.8/proyecto10.8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto10._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int numero;
+
+                for (int i = 0; i < 50; i++)
+                {
+                    numero = i + 1;
+                    Console.WriteLine(numero+" x 5 = "+(numero*5));
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un dato invalido");
+            }
+            Console.ReadKey();
+        }
+    }
+}
